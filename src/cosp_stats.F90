d666f49510fdf4d8834dbf3e29f37554dc0b702e
Charlotte Beall <cbeall@ucsd.edu>
2021-05-03 14:59:53 -0700

    Revert "Further changes to cosp_stats.F90 for compatibility with native EAM grid"
    This reverts commit 7619df7971e9f5b877fdc9f44ad7e373286e56ea.

--- src/cosp_stats.F90
+++ src/cosp_stats.F90
@@ -102,18 +102,13 @@ SUBROUTINE COSP_CHANGE_VERTICAL_GRID(Npoints,Ncolumns,Nlevels,zfull,zhalf,y,Ngle
      oldgrid_top(Nlevels) = zfull(i,Nlevels) +  zfull(i,Nlevels) - zhalf(i,Nlevels) ! Top level symmetric
      l = 0 ! Index of level in the old grid
      Nglevels2 = Nglevels - 1
-     !print*, 'Nglevels2: ',Nglevels2
      ! Loop over levels in the new grid
-     do k = 1,Nglevels
-       !print*, 'level: ',k
+     do k = 1,Nglevels2
        Nw = 0 ! Number of weigths
        wt = 0._wp ! Sum of weights
        ! Loop over levels in the old grid and accumulate total for weighted average
-       do l = 1,Nglevels
-       !do	
-         !l = k
-	 !l = l+1 
-	 !print*, 'l level index: ',l
+       do
+         l = l + 1
          w = 0.0 ! Initialise weight to 0
          ! Distances between edges of both grids
          dbb = oldgrid_bot(l) - newgrid_bot(k)
